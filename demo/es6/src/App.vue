41fd0ad1a3ab22aff929aa2e8cb8ab017f8aa3b0
Cavanaugh Richards <hello@cavatron.net>
2018-04-12 15:39:03 -0500

    fix es6 demo not being checked into git

---
+++ demo/es6/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <VueNoVNC websockify="" />
+  </div>
+</template>
+
+<script>
+import VueNoVNC from '../../../dist/vue-novnc'
+
+export default {
+  name: 'app',
+  components: {
+    VueNoVNCkln
+  }
+}
+</script>
